ebb68b634d6b609fc49d1fe142d61ec3bb0103ab
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2023-02-03 12:33:28 +0000

    管理者商品一覧、詳細、編集ページ作成

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,15 @@ class OrderDetail < ApplicationRecord
 
   has_one_attached :image
 
+  belongs_to :order
+  belongs_to :item
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
+  def subtotal
+    item.with_tax_price * amount
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,14 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   scope module: :public do
     root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    get 'item/:id' => 'items#show', as: 'item'
+    get 'items' => 'items#index', as: 'items'
   end
   namespace :admin do
     root to: 'homes#top'
+    
+    resources :items
   end
 
 
