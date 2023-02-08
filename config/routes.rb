d695bb294cb14f9ce4311a9840d4e8af4691935f
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2023-02-08 11:02:05 +0000

    会員側の会員情報編集ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   devise_scope :customer do
     post 'customers/guest_sign_in', to: 'public/sessions#guest_sign_in'
+    post 'customers/sign_up',to: 'public/registrations#create'
   end
 
   scope module: :public do
@@ -20,12 +21,17 @@ Rails.application.routes.draw do
     get 'about' => 'homes#about', as: 'about'
     get 'item/:id' => 'items#show', as: 'item'
     get 'items' => 'items#index', as: 'items'
+    resources :items
+
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_customers'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'customer_unsubscribe'
+    patch 'customers/update' => 'customers#update', as: 'customer_update'
+    patch 'customers/withdrawal' => 'customers#withdrawal', as: 'customer_withdrawal'
+    resource :customers, only: [:show]
   end
   namespace :admin do
     root to: 'homes#top'
 
-    resources :items
-    resources :customers
   end
 
 
