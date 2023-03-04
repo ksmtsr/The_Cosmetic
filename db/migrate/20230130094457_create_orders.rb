a59bf4ae9d20b8bfd0f4ee8847e799fbee9f25a7
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2023-03-04 06:44:28 +0000

    fix migration

--- db/migrate/20230130094457_create_orders.rb
+++ db/migrate/20230130094457_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.integer :customer_id,             null: false, default: ""
+      t.integer :customer_id,             null: false
       t.integer :payment_method,          null: false
       t.integer :payment,                 null: false
       t.integer :postage,                 null: false
